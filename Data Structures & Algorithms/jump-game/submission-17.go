5c67e8ab1ca7e597cd735ef62d767642f8621f9b
Bhavana111mvn <bhavanamvnpersonal@gmail.com>
2026-06-21 22:03:41 +0530

    Add: jump-game - submission-17

---
+++ Data Structures & Algorithms/jump-game/submission-17.go
@@ -0,0 +1,31 @@
+func canJump(nums []int) bool {
+	left:= len(nums)-2
+	maxsum := make([]int, len(nums))
+	if len(nums) == 1 {
+		return true
+	}
+	if nums[0]==0 {
+		return false
+	}
+	for left >0 {
+	    //if left+nums[left] < len(nums) {
+			i:=1
+			for i <= nums[left] {
+				if left+i < len(nums) && ((i+ maxsum[left+i]) > maxsum[left]) {
+                    maxsum[left]=i+ maxsum[left+i]
+				}
+				i++
+			}
+	    //} else {
+			//maxsum[left]=nums[left]
+		//}
+		left--	
+	}
+	fmt.Println(maxsum)
+	for i:=1; i<=nums[0]; i++ {
+		if i+maxsum[i] >= len(nums)-1 {
+			return true
+		}
+	}
+    return false
+}
