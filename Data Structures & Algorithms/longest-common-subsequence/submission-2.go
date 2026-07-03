7202bcf861a2fd30cb6dd33cee6ad9941a841b8e
Bhavana111mvn <bhavanamvnpersonal@gmail.com>
2026-07-03 18:25:18 +0530

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.go
@@ -0,0 +1,29 @@
+func longestCommonSubsequence(text1 string, text2 string) int {
+    cache := make([][]int, len(text1)+1)
+    for i:= range cache {
+        cache[i] = make([]int, len(text2)+1)
+        for j:= range cache[i] {
+            cache[i][j]=-1
+        }
+    }
+    return f(text1,text2,len(text1)-1,len(text2)-1,cache)
+}
+
+
+func f(text1 string, text2 string, text1Id int, text2Id int,cache [][]int) int {
+    if text1Id < 0 || text2Id < 0 {
+        return 0
+    }
+    if cache[text1Id][text2Id] != -1 {
+        return cache[text1Id][text2Id]
+    }
+    case1, case2, case3 := 0,0,0
+    if text1[text1Id] == text2[text2Id] {
+        case1 = 1+f(text1,text2,text1Id-1,text2Id-1,cache) 
+    } 
+    case2 = 0 +f(text1,text2,text1Id, text2Id-1,cache)
+    case3 = 0 +f(text1,text2,text1Id-1, text2Id,cache)
+    cache[text1Id][text2Id]= max(case1, case2, case3)
+    return max(case1, case2, case3)
+
+}
