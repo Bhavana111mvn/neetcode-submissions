fc926ad62c14ac03aaed4e598020819585332820
Bhavana111mvn <bhavanamvnpersonal@gmail.com>
2026-07-04 10:24:04 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-5

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-5.go
@@ -0,0 +1,45 @@
+func maxProfit(prices []int) int {
+	maxi :=-1
+	for _, price := range prices {
+		maxi = max(maxi, price)
+	}
+	cache := make([][]int, len(prices)+1)
+	for i := range cache {
+		cache[i]= make([]int, max(maxi+1,len(prices)))
+		for j := range cache[i] {
+			cache[i][j] = -1
+			if i == len(prices) {
+				cache[i][j]=0
+			}
+		}
+	}
+	for ind:= len(prices)-1; ind >=0;ind-- {
+		profit := 0
+		for buy:=1; buy >=0;buy-- {
+			if buy == 0 {
+		       profit = max(-prices[ind] + cache[ind+1][1],  cache[ind+1][0])
+	        } else {
+		        profit = max(prices[ind]+ cache[ind+1][0], cache[ind+1][1])
+	        }
+			cache[ind][buy] = profit
+		}
+	}
+    return cache[0][0]
+}
+
+func f(ind, buy int, prices []int, cache [][]int) int {
+	if ind==len(prices) {
+		return 0
+	}
+	if  cache[ind][buy] != -1 {
+		return cache[ind][buy]
+	}
+	profit := 0
+	if buy == 0 {
+		profit = max(-prices[ind] + f(ind+1,1,prices, cache),  f(ind+1,0,prices, cache))
+	} else {
+		profit = max(prices[ind]+ f(ind+1,0,prices, cache),f(ind+1,1,prices, cache))
+	}
+    cache[ind][buy] = profit
+	return profit
+}
